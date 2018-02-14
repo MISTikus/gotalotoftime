700afe1964354f906d2c6ccc5b975b72a6b72b32
MTimoshinin <lord.mistikus@gmail.com>
2018-02-14 23:56:01 +0200

    feat: parse identifier and return bad request if it is bad

--- taskservice/taskservice.go
+++ taskservice/taskservice.go
@@ -6,6 +6,10 @@ import (
 
 	"log"
 
+	"strings"
+
+	"strconv"
+
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -14,18 +18,40 @@ type taskservice struct {
 }
 
 func NewService() taskservice {
-	return taskservice{
-		Routes: []common.Route{
-			{
-				Route:   "task/:id",
-				Method:  common.Get,
-				Handler: getTask,
-			},
+	service := taskservice{}
+	service.Routes = []common.Route{
+		{
+			Route:   "task/:id",
+			Method:  common.Get,
+			Handler: service.getTask,
+		},
+		{
+			Route:   "task",
+			Method:  common.Post,
+			Handler: service.addTask,
 		},
 	}
+	return service
 }
 
-func getTask(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (service taskservice) getTask(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	idString := strings.TrimSpace(p.ByName("id"))
+	_, err := strconv.ParseInt(idString, 10, 32)
+	if idString == "" || err != nil {
+		badRequest(w, "Identifier '"+idString+"' can not be parsed")
+		return
+	}
+
+	log.Println("Resolving task with id: " + idString)
+	w.Write([]byte("Task with id '" + idString + "' is found"))
+}
+
+func badRequest(w http.ResponseWriter, message string) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
+}
+
+func (service taskservice) addTask(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	log.Println(r)
 	log.Println(p)
 	w.Write([]byte("new task"))
